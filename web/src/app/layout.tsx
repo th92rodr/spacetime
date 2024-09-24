7469b0a44b1f94d1b3d9711f721a313a6dfb0515
th92rodr <thiago.clrodrigues@gmail.com>
2024-09-24 12:59:49 -0300

    start nextjs react project

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from 'next'
+import type React from 'react'
+
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Spacetime',
+  description: '',
+}
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={'antialiased'}>{children}</body>
+    </html>
+  )
+}
