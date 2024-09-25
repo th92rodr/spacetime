3d30634293dc794a0d2df895e46ac6aef57bb113
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-24 23:09:53 -0300

    Web: set project fonts, page icon and add tailwind custom configs

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,17 +1,33 @@
 import type { Metadata } from 'next'
-import type React from 'react'
+import { Bai_Jamjuree as BaiJamjuree, Roboto_Flex as Roboto } from 'next/font/google'
+import type { ReactNode } from 'react'
 
 import './globals.css'
 
+const roboto = Roboto({
+  subsets: ['latin'],
+  variable: '--font-roboto',
+})
+
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
+
 export const metadata: Metadata = {
   title: 'Spacetime',
-  description: '',
+  description: 'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e TypeScript.',
 }
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
-      <body className={'antialiased'}>{children}</body>
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} font-sans text-gray-100 bg-gray-900`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
