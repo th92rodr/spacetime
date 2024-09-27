7451144cd00bdb41be56a291d007ca9c53a10b5b
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-27 18:28:35 -0300

    Server: create file upload route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
+import { resolve } from 'node:path'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import fastifyStatic from '@fastify/static'
 import fastify from 'fastify'
 import {
   type ZodTypeProvider,
@@ -10,6 +13,7 @@ import {
 import { env } from '@/env'
 import { authRoutes } from '@/routes/auth'
 import { memoriesRoutes } from '@/routes/memories'
+import { uploadRoutes } from '@/routes/upload'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -21,10 +25,18 @@ app.register(jwt, {
   secret: 'TOP_SECRET',
 })
 
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
+app.register(multipart)
+
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app.listen({ port: env.PORT }).then(() => {
