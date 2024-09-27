d2c67f51f5793275b544de30f5e016febdcf2fba
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-27 17:51:04 -0300

    Server: create authentication route

--- server/src/env.ts
+++ server/src/env.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   PORT: z.coerce.number().default(3333),
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
---
+++ server/src/routes/auth.ts
@@ -0,0 +1,78 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { env } from '@/env'
+import { prisma } from '@/lib/prisma'
+
+export const authRoutes: FastifyPluginAsyncZod = async app => {
+  app.route({
+    method: 'POST',
+    url: '/register',
+    schema: {
+      body: z.object({
+        code: z.string(),
+      }),
+    },
+    handler: async request => {
+      const { code } = request.body
+
+      const url = `https://github.com/login/oauth/access_token?client_id=${env.GITHUB_CLIENT_ID}&client_secret=${env.GITHUB_CLIENT_SECRET}&code=${code}`
+
+      const accessTokenResponse = await fetch(url, {
+        method: 'POST',
+        body: null,
+        headers: { Accept: 'application/json' },
+      })
+
+      interface AccessTokenResponse {
+        access_token: string
+        scope: string
+        token_type: string
+      }
+
+      const { access_token: accessToken } = await accessTokenResponse.json() as AccessTokenResponse
+
+      const userResponse = await fetch('https://api.github.com/user', {
+        method: 'GET',
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+
+      interface UserResponse {
+        id: number
+        login: string
+        name: string
+        avatar_url: string
+      }
+
+      const {
+        id: githubId,
+        login,
+        name,
+        avatar_url: avatarUrl,
+      } = (await userResponse.json()) as UserResponse
+
+      let userDb = await prisma.user.findUnique({
+        where: { githubId },
+      })
+
+      if (!userDb) {
+        userDb = await prisma.user.create({
+          data: { githubId, login, name, avatarUrl },
+        })
+      }
+
+      const token = app.jwt.sign(
+        {
+          name: userDb.name,
+          avatarUrl: userDb.avatarUrl,
+        },
+        {
+          sub: userDb.id,
+          expiresIn: '30 days',
+        }
+      )
+
+      return { token }
+    },
+  })
+}
