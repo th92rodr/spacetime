06d4b014356c958de5761b4e8910a96ccecfe34e
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-27 12:04:14 -0300

    Web: decode jwt token and create profile component

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: ['avatars.githubusercontent.com'],
+  },
+}
 
 export default nextConfig
