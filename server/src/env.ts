5a61c0363f7f0de6322cb54ce27c34e19ec06344
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-27 18:29:01 -0300

    Server: add api host env variable

--- server/src/env.ts
+++ server/src/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
+  HOST: z.string().default('localhost'),
   PORT: z.coerce.number().default(3333),
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
