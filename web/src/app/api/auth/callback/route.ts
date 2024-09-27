8add51747729ce7ef62e234e57da89c1d9a7da78
th92rodr <thiago.clrodrigues@gmail.com>
2024-09-27 17:42:58 -0300

    add authentication middleware

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -6,10 +6,13 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
 
+  const redirectTo = request.cookies.get('redirectTo')?.value
+
   const response = await api.post('/register', { code })
   const { token } = response.data
 
-  const redirectUrl = new URL('/', request.url)
+  const redirectUrl = redirectTo ?? new URL('/', request.url)
+
   const cookieExpirationInSeconds = 60 * 60 * 24 * 30 // 1 month
 
   return NextResponse.redirect(redirectUrl, {
