ce53c4c87995b85df1af77a155a82fdcd87a0188
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-27 17:40:10 -0300

    Web: move global html from home page to app layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from 'next'
 import { Bai_Jamjuree as BaiJamjuree, Roboto_Flex as Roboto } from 'next/font/google'
+import { cookies } from 'next/headers'
 import type { ReactNode } from 'react'
 
+import { Copyright } from '@/components/copyright'
+import { Hero } from '@/components/hero'
+import { Profile } from '@/components/profile'
+import { SignIn } from '@/components/sign-in'
 import './globals.css'
 
 const roboto = Roboto({
@@ -21,12 +26,27 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} font-sans text-gray-100 bg-gray-900`}
       >
-        {children}
+        <main className="grid grid-cols-2 min-h-screen">
+          <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+            <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
+
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
+
+          <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
