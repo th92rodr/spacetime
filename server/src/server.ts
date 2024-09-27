a9146691345a88d69e27ebdc33029483c8415cd9
th92rodr <thiago.clrodrigues@gmail.com>
2024-09-27 17:52:57 -0300

    create memories routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,31 @@
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 import { env } from '@/env'
+import { authRoutes } from '@/routes/auth'
+import { memoriesRoutes } from '@/routes/memories'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(cors, {
   origin: '*',
 })
+
+app.register(jwt, {
+  secret: 'TOP_SECRET',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running')
