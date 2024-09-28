c29368c0e59e4104e74fe0047e2aab79ff78980c
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-28 01:52:25 -0300

    Web: create memories listing in app home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,71 @@
-import { EmptyMemories } from '@/components/empty-memories'
+import dayjs from 'dayjs'
+import ptBr from 'dayjs/locale/pt-br'
+import { ArrowRight } from 'lucide-react'
+import { cookies } from 'next/headers'
+import Image from 'next/image'
+import Link from 'next/link'
 
-export default function Home() {
-  return <EmptyMemories />
+import { EmptyMemories } from '@/components/empty-memories'
+import { api } from '@/lib/api'
+
+dayjs.locale(ptBr)
+
+interface Memory {
+  id: string
+  cover_url: string
+  excerpt: string
+  created_at: string
+}
+
+export default async function Home() {
+  const isAuthenticated = cookies().has('token')
+
+  if (!isAuthenticated) {
+    return <EmptyMemories />
+  }
+
+  const token = cookies().get('token')?.value
+  const response = await api.get('/memories', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+  const memories: Memory[] = response.data
+
+  if (memories.length === 0) {
+    return <EmptyMemories />
+  }
+
+  return (
+    <div className="flex flex-col gap-10 p-8">
+      {memories.map(memory => {
+        return (
+          <div
+            key={memory.id}
+            className="space-y-4"
+          >
+            <time className="flex items-center gap-2 text-sm text-gray-100 -ml-8 before:h-px before:w-5 before:bg-gray-50">
+              {dayjs(memory.created_at).format('D[ de ]MMMM[, ]YYYY')}
+            </time>
+            <Image
+              src={memory.cover_url}
+              alt=""
+              width={592}
+              height={280}
+              className="w-full aspect-video object-cover rounded-lg"
+            />
+            <p className="text-lg leading-relaxed text-gray-100">{memory.excerpt}</p>
+
+            <Link
+              href={`/memories/${memory.id}`}
+              className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
+            >
+              Ler mais
+              <ArrowRight className="w-4 h-4" />
+            </Link>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
